5c532905167c9a7be163dac524b67f98f2ba324c
starkensan <starkensan@users.noreply.github.com>
2026-06-14 20:22:42 +0900

    Update default XIAO pin mapping

--- include/config.h
+++ include/config.h
@@ -1,21 +1,25 @@
 #pragma once
 
 #ifndef D1
-#define D1 4
+#define D1 2
 #endif
 
 #ifndef D2
-#define D2 15
+#define D2 3
 #endif
 
 #ifndef D3
-#define D3 16
+#define D3 4
+#endif
+
+#ifndef D8
+#define D8 7
 #endif
 
 #define IR_SEND_PIN D1
 #define IR_RECV_PIN D2
 #define CONFIG_BUTTON_PIN D3
-#define STATUS_LED_PIN -1
+#define STATUS_LED_PIN D8
 
 #define DEVICE_NAME "esp32-smart-remote"
 #define SETUP_AP_SSID "SmartRemote-Setup"
